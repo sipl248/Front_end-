287e38254c420448c54ba366b958fb79019fcb8b
Ruchit Lukhi <ruchit.15801@gmail.com>
2025-07-26 12:56:20 +0530

    add google tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -114,6 +114,14 @@ export default function RootLayout({ children }) {
           name="description"
           content="Pokiifuns: Your ultimate gaming destination! Explore a vast collection of thrilling games, from action-packed adventures to brain-teasing puzzles. Dive into a world of fun and challenge your skills on Pokiifuns today!"
         /> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FGE8FDPWWR"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FGE8FDPWWR');
+        </script>
         <link
           href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
           rel="stylesheet"
