2b546616646a32de3bf8fff47644402ddebd2a9d
Ruchit Lukhi <ruchit.15801@gmail.com>
2025-07-26 13:01:54 +0530

    fix code of build

--- src/app/layout.js
+++ src/app/layout.js
@@ -90,6 +90,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FGE8FDPWWR"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FGE8FDPWWR');
+          `}
+        </Script>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
           rel="icon"
@@ -114,14 +126,7 @@ export default function RootLayout({ children }) {
           name="description"
           content="Pokiifuns: Your ultimate gaming destination! Explore a vast collection of thrilling games, from action-packed adventures to brain-teasing puzzles. Dive into a world of fun and challenge your skills on Pokiifuns today!"
         /> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FGE8FDPWWR"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
 
-          gtag('config', 'G-FGE8FDPWWR');
-        </script>
         <link
           href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
           rel="stylesheet"
@@ -147,27 +152,16 @@ export default function RootLayout({ children }) {
           src="https://the.gatekeeperconsent.com/cmp.min.js"
           data-cfasync="false"
         ></script> */}
-        {/* <Script
-          src="//www.ezojs.com/ezoic/sa.min.js"
-          strategy="beforeInteractive"
-          async
-        />
-        <Script id="ezoic-init" strategy="beforeInteractive">
-          {`
-        window.ezstandalone = window.ezstandalone || {};
-        ezstandalone.cmd = ezstandalone.cmd || [];
-      `}
-        </Script> */}
-        {/* ✅ Google Tag Manager script */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5FC7Z8WT');
-          `}
-        </Script>
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FGE8FDPWWR"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-FGE8FDPWWR');
+          `,
+        }} />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
